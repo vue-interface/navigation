c454e787d318aef24c67ec6fbfef485877d0d000
Justin Kimbrell <jkimbrell@actengage.com>
2020-08-31 10:13:59 -0600

    pre-release commit

--- main.vue
+++ main.vue
@@ -1,10 +1,5 @@
 <script>
 import './src/css/Navigation.css';
 import Navigation from './src/Navigation';
-import BtnDropdown from '@vue-interface/btn-dropdown';
-
-export default {
-    BtnDropdown,
-    Navigation
-};
+export default Navigation;
 </script>
